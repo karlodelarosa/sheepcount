876c1bd2405f3956d585e6336392c1767e36408a
Karlo Dela Rosa <jrosa@salespond.com>
2025-09-29 02:00:09 +0800

    feat: added new designs

---
+++ app/(protected)/_components/header/header.tsx
@@ -0,0 +1,13 @@
+import { AuthButton } from "@/components/auth-button";
+import { ThemeSwitcher } from "@/components/theme-switcher";
+
+export default function Header() {
+  return (
+    <nav className="w-full flex justify-end border-b border-b-foreground/10 h-16">
+      <div className="flex items-center gap-4 px-5">
+        <ThemeSwitcher />
+        <AuthButton />
+      </div>
+    </nav>
+  )
+}
---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,22 @@
+import Sidebar from "./_components/sidebar/sidebar";
+import Header from "./_components/header/header";
+
+export default function ProtectedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="min-h-screen flex">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <div className="flex-1 flex flex-col gap-6 p-6 bg-gray-100">
+          <main className="flex-1 flex flex-col gap-6">
+            {children}
+          </main>
+        </div>
+      </div>
+    </main>
+  );
+}
